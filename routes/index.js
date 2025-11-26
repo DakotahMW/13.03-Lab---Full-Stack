c9c22442554c12571fc72d0497c12ef58d6f1742
DakotahMW <156035461+DakotahMW@users.noreply.github.com>
2025-11-26 09:39:51 +0000

    completed

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next){
       if (err) {
         console.error('Error fetching todos:', err);
         return res.status(500).send('Error fetching todos');
-      }
+      }s
       res.render('index', { title: 'My Simple TODO', todos: results });
     });
   } catch (error) {
@@ -17,6 +17,7 @@ router.get('/', function(req, res, next){
   }
 });
 
+// create task into the list
 router.post('/create', function (req, res, next) {
     const { task } = req.body;
     try {
@@ -35,6 +36,7 @@ router.post('/create', function (req, res, next) {
     }
 });
 
+// delete the task from the list
 router.post('/delete', function (req, res, next) {
     const { id } = req.body;
     try {
@@ -50,6 +52,47 @@ router.post('/delete', function (req, res, next) {
     }catch (error) {
         console.error('Error deleting todo:', error);
         res.status(500).send('Error deleting todo:');
+    }
+});
+
+
+// change the status of the task (ex: change Not Completed to Completed. Vice versa.)
+router.post('/changeStatus', function (req, res, next) {
+    const { id } = req.body;
+    try {
+      req.db.query('UPDATE todos SET completed = NOT completed WHERE id = ?;', [id], (err, results) => {
+        if (err) {
+          console.error('Error changing status todo:', err);
+          return res.status(500).send('Error deleting todo');
+        }
+        console.log('Todo changing status successfully:', results);
+        // Redirect to the home page after deletion
+        res.redirect('/');
+    });
+    }catch (error) {
+        console.error('Error changing status todo:', error);
+        res.status(500).send('Error changing status todo:');
+    }
+});
+
+/* change/update the description of the task based on the textbox
+   ex: if user click "Change Task" button without changing the text of the task,
+       won't effect the current description of the task.*/
+router.post('/changeTask', function (req, res, next) {
+    const { id, task } = req.body;
+    try {
+      req.db.query('UPDATE todos SET task = ? WHERE id = ?;', [task, id], (err, results) => {
+        if (err) {
+          console.error('Error changing status todo:', err);
+          return res.status(500).send('Error changing tasks todo');
+        }
+        console.log('Todo changing status successfully:', results);
+        // Redirect to the home page after deletion
+        res.redirect('/');
+    });
+    }catch (error) {
+        console.error('Error changing status todo:', error);
+        res.status(500).send('Error changing status todo:');
     }
 });
 
